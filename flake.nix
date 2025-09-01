3298dd9600acf11fbc0b9742f8c1294eaafb4bd1
Daniel Winkler <dw@dwinkler.org>
2025-09-01 17:54:46 +1000

    added caches

--- flake.nix
+++ flake.nix
@@ -121,4 +121,14 @@
       };
     });
   };
+  nixConfig = {
+    extra-substituters = [
+      "https://rstats-on-nix.cachix.org"
+      "https://rde.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
+      "rde.cachix.org-1:yRxQYM+69N/dVER6HNWRjsjytZnJVXLS/+t/LI9d1D4="
+    ];
+  };
 }
