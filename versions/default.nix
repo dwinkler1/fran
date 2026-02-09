aad022233657bf8c90e10eb0bc9c31320c5ee5e1
Daniel <dw@dwinkler.org>
2026-02-09 17:24:08 +1100

    Fix sed command path in default.nix

--- versions/default.nix
+++ versions/default.nix
@@ -7,5 +7,5 @@ pkgs: ''
   $pg --json s3alfisc summclust > versions/summclust.json
   $pg --json synth-inference synthdid > versions/synthdid.json
   RVER=$( wget -qO- 'https://raw.githubusercontent.com/ropensci/rix/refs/heads/main/inst/extdata/available_df.csv' | tail -n 1 | head -n 1 | cut -d',' -f4 | tr -d '"' )
-  ${pkgs.gnused}/bin/sed -i "s|rixpkgs.url = \"github:rstats-on-nix/nixpkgs/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\";|rixpkgs.url = \"github:rstats-on-nix/nixpkgs/$RVER\";|" ../flake.nix
+  ${pkgs.gnused}/bin/sed -i "s|rixpkgs.url = \"github:rstats-on-nix/nixpkgs/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\";|rixpkgs.url = \"github:rstats-on-nix/nixpkgs/$RVER\";|" flake.nix
 ''
