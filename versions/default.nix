a10f4dee0ee2020e91a8b845deabb21d17f5b76c
Daniel <dw@dwinkler.org>
2026-02-09 16:57:58 +1100

    Update rixpkgs URL to use dynamic version

--- versions/default.nix
+++ versions/default.nix
@@ -6,4 +6,6 @@ pkgs: ''
   $pg --json s3alfisc fwildclusterboot > versions/fwildclusterboot.json
   $pg --json s3alfisc summclust > versions/summclust.json
   $pg --json synth-inference synthdid > versions/synthdid.json
+  RVER=$( wget -qO- 'https://raw.githubusercontent.com/ropensci/rix/refs/heads/main/inst/extdata/available_df.csv' | tail -n 1 | head -n 1 | cut -d',' -f4 | tr -d '"' )
+  pkgs.sed -i "s|rixpkgs.url = \"github:rstats-on-nix/nixpkgs/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\";|rixpkgs.url = \"github:rstats-on-nix/nixpkgs/$RVER\";|" ../flake.nix
 ''
